a89b23136c459aa7d822afc156b46b64baacc7fa
Demmynile <balogun80@gmail.com>
2021-11-26 10:19:50 +0100

    new commit

---
+++ housepricingapi/api/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+import pickle
+import joblib
+import numpy as np
+from rest_framework.response import Response
+
+
+
+class HousepricingView(APIView):
+        
+        def post(self , request):
+            data = request.data
+
+            if data is not None:
+                bathrooms = data["bathrooms"]
+                serviced_price = data["serviced_price"]
+                new_price = data["new_price"]
+                estate_price = data["estate_price"]
+                location_rank = data["location_rank"]
+                exec_flag = data["exec_flag"]
+                terrace_flag = data["terrace_flag"]
+                toilets = data["toilets"]
+                bedrooms = data["bedrooms"]
+                location = data["location"]
+                mlM = joblib.load("C:/Users/Spicywords/Documents/projects/django projects/Projects/small/HousePricingApi/housepricingapi/api/ml_model/housePricing.pkl")
+                print(mlM)
+                mlm_predict = mlM.predict([[bathrooms , serviced_price , new_price , estate_price , location_rank , exec_flag , terrace_flag , toilets , bedrooms , location]])
+                response_dict = {"response" : np.round(mlm_predict , decimals=2)}
+                print(mlm_predict)
+                return Response(response_dict, status=200)
+            else:
+                response_dict_err = {"response" , "data is invalid"}
+                return Response(response_dict_err , status = 400)
+            
+
+  
+           
+
+    
+
+ 
+        
+
+
+
+
+
